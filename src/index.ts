285945c96c5386f8e3dbaf9411c97aa9d63e263f
gideonadeti <gideonadeti0@gmail.com>
2025-01-17 20:33:28 +0000

    Set up commander

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Welcome to Wallet Tracker CLI!");
+#!/usr/bin/env node
+
+import { Command } from "commander";
+
+const program = new Command();
+
+// Metadata
+program
+  .name("Wallet Tracker CLI")
+  .description("A CLI tool for tracking your wallet")
+  .version("1.0.0");
+
+program.parse(process.argv);
